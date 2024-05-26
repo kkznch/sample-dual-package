a192353928b3ad5449a4451fedd593873fa6eaed
kekezun <kkznch710md@gmail.com>
2024-05-26 15:20:17 +0900

    cjsからesm読み込めた

--- apps/app-cjs/index.js
+++ apps/app-cjs/index.js
@@ -4,3 +4,8 @@ const packageDual = require('package-dual');
 console.log('This is app-cjs!');
 packageCjs.say();
 packageDual.say();
+
+(async () => {
+  const packageEsm = await import('package-esm');
+  packageEsm.say();
+})();
