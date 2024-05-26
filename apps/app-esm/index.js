c9ae4984d8c6ddfed5aa3b9b80345e08fd733c3f
kekezun <kkznch710md@gmail.com>
2024-05-26 15:11:43 +0900

    esmからcjsのimportできるらしい

--- apps/app-esm/index.js
+++ apps/app-esm/index.js
@@ -1,6 +1,8 @@
 import { say as sayFromEsm } from 'package-esm';
+import { say as sayFromCjs } from 'package-cjs';
 import { say as sayFromDual } from 'package-dual';
 
 console.log('This is app-esm!');
 sayFromEsm();
+sayFromCjs();
 sayFromDual();
